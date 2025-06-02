8ce313bdb480508b4c66e09bd5de53830b750270
TacioDaito <taciokikuchi@gmail.com>
2025-06-02 17:18:48 -0300

    More navbar styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="min-w-screen min-h-screen bg-gray-950 text-gray-200">
     <header>
-      <div id="navbar" class="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-150
+      <div id="navbar" class="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-160
       transition-[background] duration-300 text-xl font-bold
       [clip-path:polygon(0%_0%,100%_0%,95%_100%,5%_100%)] flex justify-center">
         <div class="flex justify-evenly items-center w-9/10">
-          <div class="flex justify-around gap-x-3 w-50">
+          <div class="flex justify-evenly gap-x-6 w-50">
             <a href="#" class="hover:text-blue-500 transition-colors">HOME</a>
             <a href="#" class="hover:text-blue-500 transition-colors">PLANS</a>
           </div>
-          <img src="@/assets/images/movestyle.svg" alt="Icon" class="w-20 h-20"/>
-          <div class="flex justify-around gap-x-3 w-50">
+          <img src="@/assets/images/movestyle.svg" alt="Icon" class="w-20 h-20 mx-4"/>
+          <div class="flex justify-evenly gap-x-6 w-50">
             <a href="#" class="hover:text-blue-500 transition-colors">CLASSES</a>
             <a href="#" class="hover:text-blue-500 transition-colors">CONTACT</a>
           </div>

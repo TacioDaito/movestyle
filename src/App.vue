ee23552caeef7f0a3a5d290f80ccd922fef88e5d
TacioDaito <taciokikuchi@gmail.com>
2025-06-02 23:30:34 -0300

    Gradient hover on navbar

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,33 @@
   <div class="min-w-screen min-h-screen bg-gray-950 text-gray-200">
     <header>
       <div id="navbar" class="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-160
-      transition-[background] duration-300 text-xl font-bold
+      transition-[background] duration-300 text-lg
       [clip-path:polygon(0%_0%,100%_0%,95%_100%,5%_100%)] flex justify-center">
         <div class="flex justify-evenly items-center w-9/10">
           <div class="flex justify-evenly gap-x-6 w-50">
-            <a href="#" class="hover:text-blue-500 transition-colors">HOME</a>
-            <a href="#" class="hover:text-blue-500 transition-colors">PLANS</a>
+            <div class="relative inline-block">
+              <a href="#" class="relative z-10 hover:bg-gradient-to-r hover:from-sky-300 hover:via-purple-300 hover:to-pink-300
+              hover:text-transparent hover:bg-clip-text transition-colors duration-300">HOME</a>
+              <span class="absolute left-0 top-0 z-0">HOME</span>
+            </div>
+            <div class="relative inline-block">
+              <a href="#" class="relative z-10 hover:bg-gradient-to-r hover:from-sky-300 hover:via-purple-300 hover:to-pink-300
+              hover:text-transparent hover:bg-clip-text transition-colors duration-300">PLANS</a>
+              <span class="absolute left-0 top-0 z-0">PLANS</span>
+            </div>
           </div>
           <img src="@/assets/images/movestyle.svg" alt="Icon" class="w-20 h-20 mx-4"/>
           <div class="flex justify-evenly gap-x-6 w-50">
-            <a href="#" class="hover:text-blue-500 transition-colors">CLASSES</a>
-            <a href="#" class="hover:text-blue-500 transition-colors">CONTACT</a>
+            <div class="relative inline-block">
+              <a href="#" class="relative z-10 hover:bg-gradient-to-r hover:from-sky-300 hover:via-purple-300 hover:to-pink-300
+              hover:text-transparent hover:bg-clip-text transition-colors duration-300">CLASSES</a>
+              <span class="absolute left-0 top-0 z-0">CLASSES</span>
+            </div>
+            <div class="relative inline-block">
+              <a href="#" class="relative z-10 hover:bg-gradient-to-r hover:from-sky-300 hover:via-purple-300 hover:to-pink-300
+              hover:text-transparent hover:bg-clip-text transition-colors duration-300">CONTACT</a>
+              <span class="absolute left-0 top-0 z-0">CONTACT</span>
+            </div>
           </div>
         </div>
       </div>

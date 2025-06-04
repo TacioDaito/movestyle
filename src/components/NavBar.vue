752bc1231bd0d75405d2cbf8b2fec8c1b91f58ff
TacioDaito <taciokikuchi@gmail.com>
2025-06-04 11:36:40 -0300

    navbar work

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="navbar fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-160
+  <div class="navbar fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-170
               transition-[background] duration-300 text-lg
-              [clip-path:polygon(0%_0%,100%_0%,95%_100%,5%_100%)] flex justify-center">
+              [clip-path:polygon(0%_0%,100%_0%,88%_100%,12%_100%)] flex justify-center
+              ">
     <div class="flex justify-evenly items-center w-9/10">
       <slot></slot>
     </div>
@@ -13,7 +14,8 @@
 
   const handleScroll = () => {
     const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('bg-black/50', window.scrollY > 10);
+    navbar.classList.toggle('bg-black/30', window.scrollY > 10);
+    navbar.classList.toggle('bg-[repeating-linear-gradient(45deg,_transparent,_transparent_10px,_rgba(128,128,128,0.3)_10px,_rgba(128,128,128,0.3)_11px,_transparent_12px,_transparent_50px)]', window.scrollY > 50);
   };
 
   onMounted(() => {
--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,8 +1,10 @@
 <template>
   <a :href="to" class="navlink hover:text-transparent hover:scale-120
-  hover:text-shadow-md/20 hover:text-shadow-purple-300 bg-clip-text
-  bg-gradient-to-r from-sky-300 via-purple-300 to-pink-300 transition-all
-  duration-200 text-gray-200 font-medium text-shadow-md/70">
+  hover:text-shadow-lg/20 hover:text-shadow-purple-400 bg-clip-text
+  bg-linear-[90deg,#8ff8ff,#a58fff_16%,#ff8fd8_33%,#a58fff_49%,#8ff8ff_66%,#a58fff_82%,#ff8fd8]
+  bg-repeat-x bg-[length:300%_auto] animate-gradient transition-all
+  duration-200 text-gray-200 font-medium text-shadow-md/70
+  active:animate-none">
     <slot></slot>
   </a>
 </template>

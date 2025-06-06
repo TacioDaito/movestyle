392a009dbb9bcea4364772283d7e26d73a93edc3
TacioDaito <taciokikuchi@gmail.com>
2025-06-06 17:01:11 -0300

    home content work

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,19 +1,45 @@
 <template>
-    <div>
-        <span class="text-7xl text-gray-200 font-bold
-        text-shadow-lg/80">
-            FROM BEGINNING TO
-        </span>
-        <span class="text-8xl font-bold font-permanent-marker
-        italic bg-gradient-to-br drop-shadow-lg/90
-        from-red-500 to-pink-500 to-80% w-90 inline-block
-        bg-clip-text text-transparent">
-            BEAST
-        </span>
+    <div class="italic text-8xl font-bold mt-20 drop-shadow-lg/90">
+        <div class="animate-move-in animate-move-in-from-left"
+        style="--tw-move-x: -50px; --tw-move-time: 1s;">
+            <span class="text-gray-200
+            text-shadow-lg/80">
+                FROM
+            </span>
+            <span class="bg-gradient-to-br
+            from-sky-500 to-violet-500 to-80% inline-block
+            bg-clip-text text-transparent">
+                BEGINNING
+            </span>
+        </div>
+        <div class="ml-31 animate-move-in animate-move-in-from-right"
+        style="--tw-move-x: 50px; --tw-move-time: 1s;">
+            <span class="text-gray-200">
+                TO
+            </span>
+            <span class="bg-gradient-to-br
+            from-red-500 to-pink-500 to-80% w-90 inline-block
+            bg-clip-text text-transparent">
+                BEAST
+            </span>
+        </div>
     </div>
-    <div>
-        <span class="text-4xl text-gray-200 font-bold">
-            We’ve Got You Covered
-        </span>
+    <div class="animate-move-in" style="--tw-move-time: 1s;">
+        <div class="flex justify-center mt-8">
+            <span class="text-4xl text-gray-200 font-bold
+            text-shadow-lg/80">
+                We’ve Got You Covered
+            </span>
+        </div>
+        <div class="flex justify-center">
+            <div class=" text-3xl my-10 w-50
+            dashed-div p-2 flex items-center justify-center">
+                <NavLink to="#">Start Now</NavLink>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+    import NavLink from './NavLink.vue';
+</script>
--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,9 +1,9 @@
 <template>
-  <a :href="to" class="navlink hover:text-transparent hover:scale-120
+  <a :href="to" class="hover:text-transparent hover:scale-120
   hover:text-shadow-lg/20 hover:text-shadow-purple-400 bg-clip-text
   bg-linear-[90deg,#8ff8ff,#a58fff_16%,#ff8fd8_33%,#a58fff_49%,#8ff8ff_66%,#a58fff_82%,#ff8fd8]
   bg-repeat-x bg-[length:300%_auto] animate-gradient transition-all
-  duration-200 text-gray-200 font-medium text-shadow-md/70
+  duration-200 text-gray-200 font-semibold text-shadow-md/70
   active:animate-none">
     <slot></slot>
   </a>

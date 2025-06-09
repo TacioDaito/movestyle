a84973f4b24f7dfedcb17a9bd19c3830709f3b44
TacioDaito <taciokikuchi@gmail.com>
2025-06-09 17:18:46 -0300

    navbar changes

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -2,7 +2,7 @@
   <a :href="to" class="hover:text-transparent hover:scale-120
   hover:text-shadow-lg/20 hover:text-shadow-purple-400 gradient-text
   transition-all duration-200 text-gray-200 font-semibold
-  text-shadow-md/70 active:animate-none">
+  text-shadow-md/70 active:animate-none px-2 pb-0.2">
     <slot></slot>
   </a>
 </template>

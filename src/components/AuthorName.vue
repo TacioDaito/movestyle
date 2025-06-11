f85e88273dfebf5b07cf45199c207734bb705a31
TacioDaito <taciokikuchi@gmail.com>
2025-06-11 11:37:52 -0300

    Responsivity to home content

--- src/components/AuthorName.vue
+++ src/components/AuthorName.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50
-    text-center text-sm text-gray-200 dashed-div p-2 my-2
-    animate-move-in" style="--tw-move-y: 10px;">
+    <div
+        class="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50
+        text-center text-xs md:text-sm text-gray-200 dashed-div p-2 my-2
+        animate-move-in"
+        style="--tw-move-y: 10px;"
+    >
         © 2025 Tacio Kikuchi
     </div>
 </template>
--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,55 +1,54 @@
 <template>
     <HomeBackground class="gap-12">
 
-        <div class="italic text-8xl font-bold drop-shadow-lg/90 ">
-
-            <div class="animate-move-in"
-            style="--tw-move-x: -50px; --tw-move-time: 1s;">
-
-                <span class="text-gray-200">
-                    FROM
-                </span>
-
-                <span class="bg-gradient-to-br from-sky-500
-                to-violet-500 to-80% bg-clip-text
-                text-transparent pr-2">
+        <div
+            class="mt-10 gap-2 italic text-6xl md:text-7xl lg:text-8xl font-bold drop-shadow-lg/90 flex flex-col items-center"
+        >
+            <div
+                class="gap-2 animate-move-in flex flex-col md:flex-row items-center"
+                style="--tw-move-x: -50px; --tw-move-time: 1s;"
+            >
+                <span class="text-gray-200">FROM</span>
+                <span
+                    class="px-2 bg-gradient-to-br from-sky-500 to-violet-500 to-80% bg-clip-text text-transparent"
+                >
                     BEGINNING
                 </span>
-                
             </div>
 
-            <div class="ml-31 animate-move-in"
-            style="--tw-move-x: 50px; --tw-move-time: 1s;">
-
-                <span class="text-gray-200">
-                    TO
-                </span>
-
-                <span class="bg-gradient-to-br from-red-500
-                to-pink-500 to-80% bg-clip-text
-                text-transparent pr-2">
+            <div
+                class="gap-2 animate-move-in flex flex-col md:flex-row items-center"
+                style="--tw-move-x: 50px; --tw-move-time: 1s;"
+            >
+                <span class="text-gray-200">TO</span>
+                <span
+                    class="px-2 bg-gradient-to-br from-red-500 to-pink-500 to-80% bg-clip-text text-transparent"
+                >
                     BEAST
                 </span>
-
             </div>
-
         </div>
 
-        <div class="animate-move-in font-bold flex flex-col items-center
-        gap-10"
-        style="--tw-move-time: 1s;">
-
-            <span class="text-4xl text-gray-200 text-shadow-lg/80">
+        <div
+            class="gap-12 animate-move-in font-bold flex flex-col items-center"
+            style="--tw-move-time: 1s;"
+        >
+            <span
+                class="text-2xl md:text-3xl lg:text-4xl text-gray-200 text-shadow-lg/40 text-center"
+            >
                 We’ve Got You Covered
             </span>
-            
-            <div class="bg-black/30 p-3 backdrop-blur-xl rounded-md">
-                <a href="#" class="gradient-text text-transparent
-                font-bold text-2xl p-1">START NOW</a>
+
+            <div class="p-3 bg-black/50 backdrop-blur-md rounded-md">
+                <a
+                    href="#"
+                    class="p-1 font-bold text-xl md:text-2xl lg:text-3xl gradient-text text-transparent"
+                >
+                    START NOW
+                </a>
             </div>
 
         </div>
-
     </HomeBackground>
 </template>
 
